9579a5d8003f745034e093979c6b09cb3d46f7de
NoVa Customizer <ksubroto42@gmail.com>
2021-01-04 17:47:49 -0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,8 +87,9 @@ TW_SCREEN_BLANK_ON_BOOT := true
 TW_THEME := portrait_hdpi
 TW_USE_TOOLBOX := true
 TW_INCLUDE_REPACKTOOLS := true
+TW_INCLUDE_REPACKTOOLS := true 
+TW_INCLUDE_RESETPROP := true 
+TW_INCLUDE_LIBRESETPROP := true
 
 # PBRP specific build flags
 PB_TORCH_PATH := "/sys/class/leds/led:torch_0"
-
-
