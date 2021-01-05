f0895728a3c19cda46e8512b4108348633e795ed
NoVa Customizer <ksubroto42@gmail.com>
2021-01-04 18:05:46 -0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -86,10 +86,9 @@ TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_THEME := portrait_hdpi
 TW_USE_TOOLBOX := true
-TW_INCLUDE_REPACKTOOLS := true
-TW_INCLUDE_REPACKTOOLS := true 
-TW_INCLUDE_RESETPROP := true 
-TW_INCLUDE_LIBRESETPROP := true
 
-# PBRP specific build flags
-PB_TORCH_PATH := "/sys/class/leds/led:torch_0"
+# TWRP specific build flags
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_INCLUDE_REPACKTOOLS := false 
+TW_INCLUDE_LIBRESETPROP := false
+TW_INCLUDE_RESETPROP := false
